32e1cf08758aeeb975a48e4f1e2c2cdd57fa6179
David Tertre <d.tertre@alumnos.upm.es>
2024-05-19 14:48:28 +0200

    generar pdf

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 
 from PIL import Image
 from PyPDF2 import PdfFileReader
+from fpdf import FPDF
+
 # import BytesIO
 
 
@@ -36,23 +38,33 @@ def obtain_first_page(path: str):
 
 
 
-
-
-
 def pdf2png(path: str):
     pass
 
 
+def generatePDF():
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('helvetica', size=12)
+    pdf.cell(text="hello world")
 
+    pdf.ln(4)
+    pdf.multi_cell(0, 5, text='aaaaaaaaaaaaaaaaaaaa')
+
+    pdf.ln(4)
+    pdf.image(fr'app\assets\svg\logo_ingenuity.svg',y = 60, w=30)
+
+    pdf.output("hello_world.pdf")
 
 
 
 def main():
-    path = fr'C:\dtertre59\Documents\3_PROGRAMACION\Python\image-converter\app\assets\userInfo.jpg'
-    img = image(path)
-    img2 = i_resize(img, 465, verbose= True)
-    i_size(img2, verbose=True)
-    i_save(img2, './app/assets/userInfo2.jpg')
+    # path = fr'C:\dtertre59\Documents\3_PROGRAMACION\Python\image-converter\app\assets\userInfo.jpg'
+    # img = image(path)
+    # img2 = i_resize(img, 465, verbose= True)
+    # i_size(img2, verbose=True)
+    # i_save(img2, './app/assets/userInfo2.jpg')
+    generatePDF()
 
 
 
